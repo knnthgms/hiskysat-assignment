07dd226dfc7cc10d49a56572480c88fd0bd7d45a
kengomes <knnthgms@gmail.com>
2020-08-26 13:49:11 +0100

    functional sorting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import data from "./data/clients.json";
+import { groupBy } from "./utils";
 import SelectableList from "./components/SelectableList";
 
 function App() {
@@ -11,25 +12,53 @@ function App() {
   const countryList = getUnique(customerList, "Country");
   const cityList = getUnique(customerList, "City");
   const companyList = getUnique(customerList, "CompanyName");
-  const [selectedCountry, setCountry] = useState();
-  const [selectedCity, setCity] = useState();
-  const [selectedCompany, setCompany] = useState();
+
+  const groupByCountry = groupBy("Country");
+  const groupedCountries = groupByCountry(customerList);
+  const sortedCountries = Object.entries(groupedCountries)
+    .sort((a, b) => a[1] - b[1])
+    .map((e) => e[0]);
+  const [selectedCountry, setCountry] = useState(sortedCountries[0]);
+
+  const groupByCity = groupBy("City");
+  const groupedCities = groupByCity(
+    customerList.filter((a) => a.Country == selectedCountry)
+  );
+  const sortedCities = Object.entries(groupedCities)
+    .sort((a, b) => a[1] - b[1])
+    .map((e) => e[0]);
+  const [selectedCity, setCity] = useState(sortedCities[0]);
+
+  const groupByCompany = groupBy("CompanyName");
+  const groupedCompany = groupByCompany(
+    customerList.filter(
+      (a) => a.Country == selectedCountry && a.City == selectedCity
+    )
+  );
+  const sortedCompanies = Object.entries(groupedCompany)
+    .sort((a, b) => a[1] - b[1])
+    .map((e) => e[0]);
+  const [selectedCompany, setCompany] = useState(sortedCompanies[0]);
+
+  useEffect(() => {
+    //lets see what
+  });
 
   return (
     <div className="App">
       <div className="selection-card">
         <SelectableList
-          data={countryList}
+          data={sortedCountries}
           getSelection={(country) => setCountry(country)}
           listType={"Country"}
         />
         <SelectableList
-          data={cityList}
+          data={sortedCities}
           getSelection={(city) => setCity(city)}
           listType={"City"}
         />
         <SelectableList
-          data={companyList}
+          data={sortedCompanies}
           getSelection={(company) => setCompany(company)}
           listType={"CompanyName"}
         />
--- src/components/SelectableList/SelectableList.js
+++ src/components/SelectableList/SelectableList.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from "react";
 
 function Selectablelist(props) {
-  const [selectedItem, selectItem] = useState(props.data[1]);
+  const selectedKey = props.listType;
+  //   const [selectedItem, selectItem] = useState(props.data[1][selectedKey]);
+  const [selectedItem, selectItem] = useState();
   const handleChange = (e) => {
     selectItem(e.target.textContent);
   };
   const listItems = props.data.map((d, i) => (
     <li className="listItem" onClick={(e) => handleChange(e)} key={i}>
+      {/* {d[selectedKey]} */}
       {d}
     </li>
   ));
